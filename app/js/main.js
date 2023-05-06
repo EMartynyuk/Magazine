449ad7800a8ba40e5bd542ec0f5d74a832cdd63f
EMartynyuk <martynyukei21@gmail.com>
2023-05-06 16:38:05 +0300

    add team

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,13 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 3000
     });
+    $('.team__slider').slick({
+        dots: true,
+        fade: true,
+        // autoplay: true,
+        // autoplaySpeed: 3000
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/team-sliderarrowleft.svg"></img></button>',
+         nextArrow: '<button type="button" class="slick-next"><img src="../images/team-sliderarrowright.svg"></img></button>' 
+
+    });
 })
