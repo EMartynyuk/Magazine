1aae527607eab15018569ab61ff19e5a0c41fbb4
EMartynyuk <martynyukei21@gmail.com>
2023-05-08 20:46:05 +0300

    fix slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,28 @@
-$(function(){
-    var mixer = mixitup('.shop__items');
-    $('.slider__carousel').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 3000
-    });
-    $('.team__slider').slick({
-        dots: true,
-        fade: true,
-        // autoplay: true,
-        // autoplaySpeed: 3000
-        prevArrow: '<button type="button" class="slick-prev"><img src="../images/team-sliderarrowleft.svg"></img></button>',
-        nextArrow:  '<button type="button" class="slick-next"><img src="../images/team-sliderarrowright.svg"></img></button>'
-    });
-    $('.star').rateYo({
-        rating: 4,
-        starWidth: "12px",
-        normalFill: "#998e78",
-        ratedFill: "#6e9c9f",
-        readOnly: true
-      });
-})
+$(function () {
+  
+  $(".slider__carousel").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+  $(".team__slider").slick({
+    dots: true,
+    fade: true,
+    // autoplay: true,
+    // autoplaySpeed: 3000
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../images/team-sliderarrowleft.svg"></img></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../images/team-sliderarrowright.svg"></img></button>',
+  });
+  $(".star").rateYo({
+    rating: 4,
+    starWidth: "12px",
+    normalFill: "#998e78",
+    ratedFill: "#6e9c9f",
+    readOnly: true,
+  });
+  var mixer = mixitup(".shop__items");
+});
