65d283640c9762ee239718cf31c7769e53202c27
EMartynyuk <martynyukei21@gmail.com>
2023-05-15 18:48:40 +0300

    fix

--- app/js/main.js
+++ app/js/main.js
@@ -16,9 +16,9 @@ $(function () {
     // autoplay: true,
     // autoplaySpeed: 3000
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../images/team-sliderarrowleft.svg"></img></button>',
+      '<button type="button" class="slick-prev"><svg width="29" height="16" viewBox="0 0 29 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M29 8L0.999999 8M0.999999 8L8.18919 0.999999M0.999999 8L8.18919 15" stroke="#858585"/></svg></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../images/team-sliderarrowright.svg"></img></button>',
+      '<button type="button" class="slick-next"><svg width="29" height="16" viewBox="0 0 29 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.34742e-08 8L28 8M28 8L20.8108 15M28 8L20.8108 1" stroke="#858585"/></svg></button>',
     responsive: [
       {
         breakpoint: 1084,
